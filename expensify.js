622a1179b80cce6e48a537853ba467b756b7e71c
Scott Giminiani <scottg489@gmail.com>
2018-05-17 02:01:30 -0700

    Refactor expensify domain logic to separate file

---
+++ expensify.js
@@ -0,0 +1,53 @@
+const expensifyConfig = require('./config').expensify
+let request = require('request-promise-native');
+let fs = require('fs');
+
+async function getAllExpenses() {
+    /**
+   * TODO: https://freemarker.apache.org/docs/ref_builtins_string.html#ref_builtin_matches
+   * TODO:   See above for things that can be done in the template such as escaping values for json, decoding
+   * TODO:   html entities (debatable if we want to do this in freemarker or here) and other useful tools.
+   */
+  let reportTemplate = fs.readFileSync('expensify_template.ftl', 'utf8')
+  let form = getReportExportForm(reportTemplate);
+
+  let fileName = await request(getExpensifyRequest(form));
+  let body = await downloadExpensifyReport(fileName)
+  return JSON.parse(body)
+}
+
+async function downloadExpensifyReport(fileName) {
+  let form = getReportDownloadForm(fileName);
+
+  return await request(getExpensifyRequest(form));
+}
+
+function getExpensifyRequest(form) {
+  return {
+    url: 'https://integrations.expensify.com/Integration-Server/ExpensifyIntegrations',
+    method: 'POST',
+    body: form,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+}
+
+function getReportExportForm(reportTemplate) {
+  let reportExport = require('./report_export')
+  reportExport.credentials.partnerUserID = expensifyConfig.partnerUserID
+  reportExport.credentials.partnerUserSecret = expensifyConfig.partnerUserSecret
+  let form = 'requestJobDescription=' + JSON.stringify(reportExport)
+  form = form.concat('&template=' + encodeURIComponent(reportTemplate))
+  return form;
+}
+
+function getReportDownloadForm(fileName) {
+  let reportDownload = require('./report_download')
+  reportDownload.credentials.partnerUserID = expensifyConfig.partnerUserID
+  reportDownload.credentials.partnerUserSecret = expensifyConfig.partnerUserSecret
+  reportDownload.fileName = fileName
+  return 'requestJobDescription=' + JSON.stringify(reportDownload);
+}
+
+module.exports = {getAllExpenses}
--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 // const mintConfig = require('./test-config').mint
 // const expensifyConfig = require('./test-config').expensify
 const mintConfig = require('./config').mint
-const expensifyConfig = require('./config').expensify
-let request = require('request-promise-native');
-let fs = require('fs');
+const expensify = require('./expensify')
 const Entities = require('html-entities').XmlEntities;
 const entities = new Entities();
 
 
 async function main() {
-  /**
-   * TODO: https://freemarker.apache.org/docs/ref_builtins_string.html#ref_builtin_matches
-   * TODO:   See above for things that can be done in the template such as escaping values for json, decoding
-   * TODO:   html entities (debatable if we want to do this in freemarker or here) and other useful tools.
-   */
-  let reportTemplate = fs.readFileSync('expensify_template.ftl', 'utf8')
-  let form = getReportExportForm(reportTemplate);
-
-  let fileName = await request(getExpensifyRequest(form));
-  return downloadExpensifyReport(fileName)
+  let allExpenses = await expensify.getAllExpenses()
+  return searchMint(allExpenses)
 }
 
-async function downloadExpensifyReport(fileName) {
-  let form = getReportDownloadForm(fileName);
-
-  let body = await request(getExpensifyRequest(form));
-  return searchMint(body)
-}
-
-async function searchMint(body) {
-  let expenses = JSON.parse(body)
-  // console.log(expenses)
+async function searchMint(allExpenses) {
   let mint = await require('pepper-mint')(mintConfig.username, mintConfig.password, mintConfig.ius_session, mintConfig.thx_guid)
   let allTrans = await getAllTransactions(mint, 0)
-  return await expenses.map((expense) => {
+  return await allExpenses.map((expense) => {
     let transactionSearchResults = allTrans.filter((transaction) => {
       return areEqual(transaction, expense)
     })
@@ -133,34 +114,6 @@ function getAllTransactionsQuery(offset) {
     startDate: "04/02/2017",
     endDate: "01/01/9999"
   };
-}
-
-function getExpensifyRequest(form) {
-  return {
-    url: 'https://integrations.expensify.com/Integration-Server/ExpensifyIntegrations',
-    method: 'POST',
-    body: form,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  };
-}
-
-function getReportExportForm(reportTemplate) {
-  let reportExport = require('./report_export')
-  reportExport.credentials.partnerUserID = expensifyConfig.partnerUserID
-  reportExport.credentials.partnerUserSecret = expensifyConfig.partnerUserSecret
-  let form = 'requestJobDescription=' + JSON.stringify(reportExport)
-  form = form.concat('&template=' + encodeURIComponent(reportTemplate))
-  return form;
-}
-
-function getReportDownloadForm(fileName) {
-  let reportDownload = require('./report_download')
-  reportDownload.credentials.partnerUserID = expensifyConfig.partnerUserID
-  reportDownload.credentials.partnerUserSecret = expensifyConfig.partnerUserSecret
-  reportDownload.fileName = fileName
-  return 'requestJobDescription=' + JSON.stringify(reportDownload);
 }
 
 module.exports = main
