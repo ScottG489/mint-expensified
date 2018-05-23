226dcf2f785d72e0634dbf503603215a6ae15bed
Scott Giminiani <scottg489@gmail.com>
2018-05-23 00:12:38 -0700

    Add timeout to ignored test
    This test is currently just being used as a manual runner to test code

--- test/integration-test.js
+++ test/integration-test.js
@@ -8,6 +8,7 @@ chai.should();
 
 describe('mint-expensified', function () {
   xit('runs', async function () {
+    this.timeout(30000);
     // TODO: Have this actually assert something
     let foo = await minty()
   })
