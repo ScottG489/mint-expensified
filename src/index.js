fce43623b197ad41f9d51cd9a4d2e9f311f0e091
Scott Giminiani <scottg489@gmail.com>
2018-06-14 01:46:10 -0700

    Refactor index file to only have orchestration logic
    Now contains code to orchestrate matcher with input of all transactions
    and expenses. It then prints the results of the matches.

--- src/index.js
+++ src/index.js
@@ -1,105 +1,63 @@
-let comparator = new require('../src/expenseToTransactionComparator')()
+const mintConfig = require('../test-config').mint
+const mint = new require('../src/mint/mint')(mintConfig)
 
-let mint
-let expensify
-let allTags
+let expensifyConfig = require('../test-config').expensify
+const expensify = new require('../src/expensify/expensify')(expensifyConfig)
 
-// async function main() {
-//   let allExpenses = expensify.getAllExpenses()
-//   let allTrans = mint.getAllTransactions()
-//   let tags = await mint.getTags()
-//   let matches = getMatchResults(await allTrans, await allExpenses)
-//   return match(await allTrans, await allExpenses)
-// }
+let matcher = new require('../src/matcher')(mint, expensify)
 
-Minty.prototype.tagMatchingTransactions = async function (allTrans, allExpenses) {
-  return await Promise.all(
-    this.getMatchResults(allTrans, allExpenses)
-    .map(async (results) => {
-      if (!results.expenseAmountWasAltered) {
-        let tag = allTags.find((tag) => {
-          return tag.value === "Vacation"
-        })
+async function main() {
+  await mint.init()
+  await matcher.init()
+  let allExpenses = expensify.getAllExpenses()
+  let allTrans = mint.getAllTransactions()
+  let matchResults = matcher.getMatchResults(await allTrans, await allExpenses)
 
-        await Promise.all(results.matchingTransactions.map(async (transaction) => {
-            return await mint.editTransaction({
-              id: transaction.id,
-              tags: [tag.id]
-            })
-          })
-        )
-      }
-
-      return results
-    })
-  )
+  printExpensesWithNoMatchingTransactions(matchResults)
+  printExpensesWithModifiedAmount(matchResults)
+  printMatchResults(matchResults)
 }
 
-Minty.prototype.getMatchResults = function(allTrans, allExpenses) {
-  return allExpenses
-    .filter((expense) => {
-      return !expense.reportName.endsWith("Transit/Mobile/Internet")
+function printMatchResults(matchResults) {
+  matchResults.forEach((result) => {
+    console.log(result.expense.merchant + " | " + result.expense.amount + " | " + result.expense.created)
+    result.matchingTransactions.forEach((transaction) => {
+      console.group()
+      console.log(transaction.omerchant + " | " + transaction.odate + " | " + transaction.amount)
+      console.groupEnd()
     })
-    .map((expense) => {
-      let transactionSearchResults = allTrans.filter((transaction) => {
-        return comparator.areEqual(transaction, expense)
-      })
-
-      return {
-        expense: expense,
-        matchingTransactions: transactionSearchResults,
-        expenseAmountWasAltered: expense.modifiedAmount !== "" && expense.amount !== expense.modifiedAmount
-      }
-    })
+  })
+  console.log()
 }
 
-async function match(allTrans, allExpenses) {
-  return allExpenses
-    .filter((expense) => {
-      return !expense.reportName.endsWith("Transit/Mobile/Internet")
+function printExpensesWithNoMatchingTransactions(matchResults) {
+  console.log("No matching transaction found for expenses:")
+
+  console.group()
+  matchResults
+    .filter((result) => {
+      return result.matchingTransactions.length === 0
     })
-    .map((expense) => {
-      let transactionSearchResults = allTrans.filter((transaction) => {
-        return comparator.areEqual(transaction, expense)
-      })
-      if (expense.modifiedAmount !== "" && expense.amount !== expense.modifiedAmount) {
-        console.log("Expense with a modified amount. This likely means that it was only partially reimbursed and you should split this transaction in mint")
-        console.log(expense)
-      }
-      if (transactionSearchResults.length > 1) {
-        /**
-         * TODO: We need to handle expenses which have multiple matching transactions. This is somewhat valid. If something
-         * TODO:   is purchased from the same merchant, on the same day, for the same amount then this will occur. I believe
-         * TODO:   the best solution here is to simply iterate over these transactions and tag them as reimbursable. The only downside
-         * TODO:   is that these transactions will get updated twice each which isn't a big deal.
-         */
-        console.log("Expense with multiple matching transactions")
-        console.log(expense)
-        console.log(transactionSearchResults)
-      } else if (transactionSearchResults.length < 1) {
-        /**
-         * TODO: This is generally bad. However, currently there are a lot of manually created expenses for monthly allowances (internet, phone, etc)
-         * TODO:   Find a way to filter out these known expenses so that this shows legitimate cases where there was no match
-         */
-        console.log("No matching transaction found for expense: ")
-        console.log(`Expected 1 result but got ${transactionSearchResults.length}`)
-        console.log(expense)
-      }
-      return expense
+    .forEach((result) => {
+    console.log(result.expense.merchant + " | " + result.expense.amount + " | " + result.expense.created)
+  })
+  console.groupEnd()
+  console.log()
+}
+
+function printExpensesWithModifiedAmount(matchResults) {
+  console.log("Expenses with a modified amount. This likely means that it was only partially reimbursed and you should split this transaction in mint")
+
+  console.group()
+  matchResults
+    .filter((result) => {
+      return result.expense.modifiedAmount !== "" && result.expense.amount !== result.expense.modifiedAmount
     })
+    .forEach((result) => {
+      console.log(result.expense.merchant + " | " + result.expense.amount + " | " + result.expense.created)
+    })
+  console.groupEnd()
+  console.log()
 }
 
-
-function Minty() {}
-// TODO: I don't like how you need to call init() after class instantiation
-Minty.prototype.init = async function() {
-  allTags = await mint.getTags()
-}
-
-function init(mintClient, expensifyClient) {
-  mint = mintClient
-  expensify = expensifyClient
-  return new Minty()
-}
-
-module.exports = init
+main()
